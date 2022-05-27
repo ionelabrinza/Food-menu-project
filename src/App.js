5b6a08f689224311a2c13c7b0c32f552b4bd3eb6
Nicoleta Brinza <ionela.brinza@yahoo.com>
2022-05-26 22:15:44 -0500

    Built menu

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+            <h2>Our Menu</h2>
+            <div className="underline"></div>
+        </div>
+        <Categories />
+        <Menu items={menuItems}/>
+      </section>
+    </main>
+  );
+}
+
+export default App;
